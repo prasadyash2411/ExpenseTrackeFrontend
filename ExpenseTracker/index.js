765995cab624af944a064dccbaa937f00871ee92
yash prasad <pyash@athenahealth.com>
2021-06-14 18:55:06 +0530

    Added payment button

--- ExpenseTracker/index.js
+++ ExpenseTracker/index.js
@@ -1,3 +1,4 @@
+const token = localStorage.getItem('token');
 function addNewExpense(e){
     e.preventDefault();
     const form = new FormData(e.target);
@@ -8,7 +9,6 @@ function addNewExpense(e){
         category: form.get("category")
 
     }
-    const token = localStorage.getItem('token');
     console.log(expenseDetails)
     axios.post('http://localhost:3000/user/addexpense',expenseDetails, { headers: {"Authorization" : token} }).then((response) => {
 
@@ -23,7 +23,6 @@ function addNewExpense(e){
 }
 
 window.addEventListener('load', ()=> {
-    const token = localStorage.getItem('token');
     axios.get('http://localhost:3000/user/getexpenses', { headers: {"Authorization" : token} }).then(response => {
         if(response.status === 200){
             response.data.expenses.forEach(expense => {
@@ -49,7 +48,6 @@ function addNewExpensetoUI(expense){
 }
 
 function deleteExpense(e, expenseid) {
-    const token = localStorage.getItem('token');
     axios.delete(`http://localhost:3000/user/deleteexpense/${expenseid}`, { headers: {"Authorization" : token} }).then((response) => {
 
     if(response.status === 204){
@@ -69,4 +67,50 @@ function showError(err){
 function removeExpensefromUI(expenseid){
     const expenseElemId = `expense-${expenseid}`;
     document.getElementById(expenseElemId).remove();
+}
+
+
+
+document.getElementById('rzp-button1').onclick = async function (e) {
+    const response  = await axios.get('http://localhost:3000/purchase/premiummembership', { headers: {"Authorization" : token} });
+    console.log(response);
+    var options =
+    {
+     "key": response.data.key_id, // Enter the Key ID generated from the Dashboard
+     "name": "Test Company",
+     "order_id": response.data.order.id, // For one time payment
+     "prefill": {
+       "name": "Test User",
+       "email": "test.user@example.com",
+       "contact": "7003442036"
+     },
+     "theme": {
+      "color": "#3399cc"
+     },
+     // This handler function will handle the success payment
+     "handler": function (response) {
+         console.log(response);
+         axios.post('http://localhost:3000/purchase/updatetransactionstatus',{
+             order_id: options.order_id,
+             payment_id: response.razorpay_payment_id,
+         }, { headers: {"Authorization" : token} }).then(() => {
+             alert('You are a Premium User Now')
+         }).catch(() => {
+             alert('Something went wrong. Try Again!!!')
+         })
+     },
+  };
+  const rzp1 = new Razorpay(options);
+  rzp1.open();
+  e.preventDefault();
+
+  rzp1.on('payment.failed', function (response){
+  alert(response.error.code);
+  alert(response.error.description);
+  alert(response.error.source);
+  alert(response.error.step);
+  alert(response.error.reason);
+  alert(response.error.metadata.order_id);
+  alert(response.error.metadata.payment_id);
+ });
 }
