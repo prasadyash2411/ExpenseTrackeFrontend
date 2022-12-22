5dc76f6f3ecf6ebb6a18359da6f4932cccdce805
prasadyash2411 <prasadyash549>
2022-12-22 08:36:19 +0530

    Crud crud integrated to restaurant app

--- ExpenseTracker/index.js
+++ ExpenseTracker/index.js
@@ -8,43 +8,22 @@ function addNewExpense(e){
 
     }
     console.log(expenseDetails)
-    const token  = localStorage.getItem('token')
-    axios.post('http://localhost:3000/expense/addexpense',expenseDetails,  { headers: {"Authorization" : token} })
+    // const token  = localStorage.getItem('token')
+    axios.post('https://crudcrud.com/api/892e97cc02c54028a49e690520aed8f4/orders',expenseDetails )
         .then((response) => {
+    localStorage.setItem(response.data._id, JSON.stringify(expenseDetails))
+    console.log(response)
+    addNewExpensetoUI(response.data);
 
-        addNewExpensetoUI(response.data.expense);
-
-    }).catch(err => showError(err))
+     }).catch(err => showError(err))
 
 }
 
-function showPremiumuserMessage() {
-    document.getElementById('rzp-button1').style.visibility = "hidden"
-    document.getElementById('message').innerHTML = "You are a premium user "
-}
-
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-}
 
 window.addEventListener('DOMContentLoaded', ()=> {
-    const token  = localStorage.getItem('token')
-    const decodeToken = parseJwt(token)
-    console.log(decodeToken)
-    const ispremiumuser = decodeToken.ispremiumuser
-    if(ispremiumuser){
-        showPremiumuserMessage()
-        showLeaderboard()
-    }
-    axios.get('http://localhost:3000/expense/getexpenses', { headers: {"Authorization" : token} })
+    axios.get('https://crudcrud.com/api/892e97cc02c54028a49e690520aed8f4/orders')
     .then(response => {
-            response.data.expenses.forEach(expense => {
+            response.data.forEach(expense => {
 
                 addNewExpensetoUI(expense);
             })
@@ -54,84 +33,25 @@ window.addEventListener('DOMContentLoaded', ()=> {
 });
 
 function addNewExpensetoUI(expense){
-    const parentElement = document.getElementById('listOfExpenses');
-    const expenseElemId = `expense-${expense.id}`;
+    const parentElement = document.getElementById(expense.category);
+    const expenseElemId = `expense-${expense._id}`;
     parentElement.innerHTML += `
         <li id=${expenseElemId}>
             ${expense.expenseamount} - ${expense.category} - ${expense.description}
-            <button onclick='deleteExpense(event, ${expense.id})'>
-                Delete Expense
+            <button onclick='deleteExpense("${expense._id}")'>
+                Delete Order
             </button>
         </li>`
 }
 
-function deleteExpense(e, expenseid) {
-    const token = localStorage.getItem('token')
-    axios.delete(`http://localhost:3000/expense/deleteexpense/${expenseid}`,  { headers: {"Authorization" : token} }).then(() => {
-
-            removeExpensefromUI(expenseid);
-
-    }).catch((err => {
-        showError(err);
-    }))
+function deleteExpense( expenseid) {
+    removeExpensefromUI(expenseid);
+    axios.delete(`https://crudcrud.com/api/892e97cc02c54028a49e690520aed8f4/orders/${expenseid}`)
+    localStorage.removeItem(expenseid)
 }
 
-function showError(err){
-    document.body.innerHTML += `<div style="color:red;"> ${err}</div>`
-}
-function showLeaderboard(){
-    const inputElement = document.createElement("input")
-    inputElement.type = "button"
-    inputElement.value = 'Show Leaderboard'
-    inputElement.onclick = async() => {
-        const token = localStorage.getItem('token')
-        const userLeaderBoardArray = await axios.get('http://localhost:3000/premium/showLeaderBoard', { headers: {"Authorization" : token} })
-        console.log(userLeaderBoardArray)
-
-        var leaderboardElem = document.getElementById('leaderboard')
-        leaderboardElem.innerHTML += '<h1> Leader Board </<h1>'
-        userLeaderBoardArray.data.forEach((userDetails) => {
-            leaderboardElem.innerHTML += `<li>Name - ${userDetails.name} Total Expense - ${userDetails.total_cost || 0} </li>`
-        })
-    }
-    document.getElementById("message").appendChild(inputElement);
-
-}
 
 function removeExpensefromUI(expenseid){
     const expenseElemId = `expense-${expenseid}`;
     document.getElementById(expenseElemId).remove();
-}
-
-document.getElementById('rzp-button1').onclick = async function (e) {
-    const token = localStorage.getItem('token')
-    const response  = await axios.get('http://localhost:3000/purchase/premiummembership', { headers: {"Authorization" : token} });
-    console.log(response);
-    var options =
-    {
-     "key": response.data.key_id, // Enter the Key ID generated from the Dashboard
-     "order_id": response.data.order.id,// For one time payment
-     // This handler function will handle the success payment
-     "handler": async function (response) {
-        const res = await axios.post('http://localhost:3000/purchase/updatetransactionstatus',{
-             order_id: options.order_id,
-             payment_id: response.razorpay_payment_id,
-         }, { headers: {"Authorization" : token} })
-        
-        console.log(res)
-         alert('You are a Premium User Now')
-         document.getElementById('rzp-button1').style.visibility = "hidden"
-         document.getElementById('message').innerHTML = "You are a premium user "
-         localStorage.setItem('token', res.data.token)
-         showLeaderboard()
-     },
-  };
-  const rzp1 = new Razorpay(options);
-  rzp1.open();
-  e.preventDefault();
-
-  rzp1.on('payment.failed', function (response){
-    console.log(response)
-    alert('Something went wrong')
- });
 }
