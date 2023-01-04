454ebc41e698370b1401c1747692fc9bbe18b5f3
prasadyash2411 <prasadyash549>
2023-01-04 09:21:14 +0530

    Total Value of products integrated

--- ExpenseTracker/index.js
+++ ExpenseTracker/index.js
@@ -4,16 +4,16 @@ function addNewExpense(e){
     const expenseDetails = {
         expenseamount: e.target.expenseamount.value,
         description: e.target.description.value,
-        category: e.target.category.value,
         orderId: e.target.orderId.value
+        // category: e.target.category.value,
 
     }
     console.log(expenseDetails)
     // const token  = localStorage.getItem('token')
-    axios.post('https://crudcrud.com/api/e96aba2f4fb04bc4a3e7b69f4dddbd14/orders',expenseDetails )
+    axios.post('https://crudcrud.com/api/8a1e1458b1834d8d86e510025a7723aa/products',expenseDetails )
         .then((response) => {
     localStorage.setItem(expenseDetails.orderId, JSON.stringify(expenseDetails))
-    
+    console.log(response)
     addNewExpensetoUI(response.data);
 
      }).catch(err => showError(err))
@@ -21,32 +21,36 @@ function addNewExpense(e){
 }
 
 
-window.addEventListener('DOMContentLoaded', ()=> {
-    axios.get('http://localhost:3000/expense/getexpenses')
-    .then(response => {
-            response.data.expenses.forEach(expense => {
+// window.addEventListener('DOMContentLoaded', ()=> {
+//     axios.get('https://crudcrud.com/api/8a1e1458b1834d8d86e510025a7723aa/products')
+//     .then(response => {
+//             response.data.forEach(expense => {
 
-                addNewExpensetoUI(expense);
-            })
-    }).catch(err => {
-        showError(err)
-    })
-});
+//                 addNewExpensetoUI(expense);
+//             })
+//     }).catch(err => {
+//         showError(err)
+//     })
+// });
 
 function addNewExpensetoUI(expense){
-    const parentElement = document.getElementById(expense.category);
+    const parentElement = document.getElementById('Products');
     const expenseElemId = `expense-${expense.orderId}`;
+    document.getElementById('totalValue').innerHTML = Number(document.getElementById('totalValue').innerHTML) + Number(expense.expenseamount)
     parentElement.innerHTML += `
         <li id=${expenseElemId}>
-            ${expense.expenseamount} - ${expense.category} - ${expense.description}
-            <button onclick='deleteExpense(event, ${expense.orderId})'>
+            ${expense.expenseamount} - ${expense.description}
+            <button onclick='deleteExpense("${expense.orderId}")'>
                 Delete Product
             </button>
         </li>`
 }
 
-function deleteExpense(e, expenseid) {
+function deleteExpense( expenseid) {
     removeExpensefromUI(expenseid);
+    axios.delete(`https://crudcrud.com/api/8a1e1458b1834d8d86e510025a7723aa/products/${expenseid}`)
+    const expense  = JSON.parse(localStorage.getItem(expenseid))
+    document.getElementById('totalValue').innerHTML = Number(document.getElementById('totalValue').innerHTML) - Number(expense.expenseamount)
     localStorage.removeItem(expenseid)
 }
 
@@ -54,4 +58,6 @@ function deleteExpense(e, expenseid) {
 function removeExpensefromUI(expenseid){
     const expenseElemId = `expense-${expenseid}`;
     document.getElementById(expenseElemId).remove();
+    
 }
+
